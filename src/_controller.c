02dfe8deec05a52827aab1bdd63ddd3720ccaac1
Bruno Pinheiro <bda-silv@student.42.rio>
2023-01-16 11:10:26 -0300

    WIP : sorting files

--- src/_controller.c
+++ src/_controller.c
@@ -1,48 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol.c                                          :+:      :+:    :+:   */
+/*   controller.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bda-silv <bda-silv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 15:42:15 by bda-silv          #+#    #+#             */
-/*   Updated: 2023/01/16 09:01:24 by bda-silv         ###   ########.fr       */
+/*   Updated: 2023/01/16 09:12:15 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
 /* TODO
- * - Enviar para router
- * * Controller
- * * TRGB
- * * Julia
- * * Refactor
  *
  * */
 
-int	main(int argc, char **argv)
+void	fractol_init(t_data *id)
 {
-	t_data	img;
-
-	if (argc == 2)
-	{
-		normalize(argv);
-		parse(argv, &img);
-		fractol_init(&img);
-		render(&img, 0x000000AA);
-		mlx_loop(img.mlx);
-	}
-	else
-		helper();
-	return (0);
+	(*id).mlx = mlx_init();
+	(*id).win = mlx_new_window(id->mlx, WIDTH, HEIGHT, "Fractol");
+	(*id).ptr = mlx_new_image(id->mlx, WIDTH, HEIGHT);
+	(*id).adr = mlx_get_data_addr(id->ptr, &id->bpp, &id->len, &id->edn);
 }
 
-/*
-	mlx_put_image_to_window(img.mlx, img.win, img.ptr, 0, 0);
-	mlx_string_put(img.mlx, img.window, 200, 200, 0x00FF0000, "Hello you!");
-
-	mlx_destroy_window(img.mlx, img.window);
-	mlx_destroy_display(&img.mlx);
-	free(img.mlx);
-*/
+void	set(t_data *i, char *t, double xmin, double xmax, double ymin, double ymax)
+{
+	(*i).type = t;
+	(*i).xmin = xmin;
+	(*i).xmax = xmax;
+	(*i).ymin = ymin;
+	(*i).ymax = ymax;
+}
